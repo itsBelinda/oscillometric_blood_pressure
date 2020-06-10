fixed ratio algorithm calculation in python working progress

python/obp_fixed_ratio.py
@@ -15,12 +15,12 @@ obp_tests.py
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
-plt.rcParams['axes.grid'] = True
+from scipy.interpolate import interp1d
 
 
 #%% Get the data
 
-data = np.loadtxt('../data/sample_07_11.dat')
+data = np.loadtxt('../data/sample_07_07.dat')
 fs= 1000 # Hz
 resolution = 24 # bits
 N = len(data)
@@ -94,42 +94,94 @@ tPumpedUP = tMaximas[xPumpedUp]
 
 deltaT = np.zeros(len(tMaximas))
 delta2T = np.zeros(len(tMaximas))
-oscValid = np.zeros(len(tMaximas))
 validCnt = 0
-oscStart = 0
-oscEnd = 0
+oscStartInd = 0
+oscEndInd = 0
+
+
+#deltaT = np.diff(tMaximas)
+#delta2T = np.diff(deltaT)
 for i in range(1,len(tMaximas)-1):
     deltaT[i] = tMaximas[i]-tMaximas[i-1]
     delta2T[i] = deltaT[i]-deltaT[i-1]
     
-    if oscStart == 0 :
+    if oscStartInd == 0 :
         # check for start of oscillogram: 
         if np.abs(delta2T[i]) < 0.1 and i > xPumpedUp :
             validCnt += 1
             if validCnt == 5 :
-                oscStart = tMaximas[i - (validCnt-1)]
+                oscStartInd = i - (validCnt-1)
         else:
             validCnt = 0
-    elif oscEnd == 0:
+    elif oscEndInd == 0:
         #check for end of oscillogram
         if np.abs(delta2T[i]) > 0.1:
-            oscEnd = tMaximas[i-1]
-        
-        
-    
-# data for processing:
-iStart = int(oscStart*1000)
-iEnd = int(oscEnd*1000)
-tP = t[iStart:iEnd]
-yhpP = yfHP[iStart:iEnd]
-ylpP = yfLP[iStart:iEnd]
+            oscEndInd = i-2 #omitt the last one: -2 not -1       
 
-# plot imporant part of singal:
+if oscEndInd == 0:
+    oscEndInd = len(tMaximas)-2
+
+# data for processing:
+tStart = tMaximas[oscStartInd]
+tEnd = tMaximas[oscEndInd]
+iStart = int(tStart*1000)
+iEnd = int(tEnd*1000)
+tMaxP = tMaximas[oscStartInd:oscEndInd+1]
+oscMaxP = oscMax[oscStartInd:oscEndInd+1]
+deltaP = deltaT[oscStartInd:oscEndInd+1]
+tP = t[iStart:iEnd+1]
+yhpP = yfHP[iStart:iEnd+1]
+ylpP = yfLP[iStart:iEnd+1]
+
+avPulse = np.average(deltaP)
+pulse=60/avPulse
+
+xMAP = np.argmax(yhpP)
+
+# Todo: check again and include minimas
+pMAP = ylpP[xMAP]
+pMAPav = np.average(ylpP[xMAP-int(avPulse*1000/2):xMAP+int(avPulse*1000/2)]) #averaged over vone pulse
+
+
+
+print("Pulse: ", int(pulse))
+print("MAP (max value): ", int(pMAP))
+print("MAP (av value over pulse len): ", int(pMAPav))
+
+
+interp = interp1d(tMaxP, oscMaxP, kind='quadratic')
+
+
+# plt.plot(ylpP, yhpP)
+# plt.figure()
+# plt.plot(tMaxP, oscMaxP)
+# plt.plot(tP, yhpP)
+# plt.plot(tP, interp(tP))
+# plt.figure()
+# plt.plot(tMaxP, oscMaxP)
+# plt.plot(tP, yhpP)
+# plt.plot(tP, interp(tP))
+# proc_delta.plot(tP, interp(tP), 'c', label='interpolate')
+
+# envelope only works for DC free signals (remove by additional filtering?)
+# plt.figure('envelope')
+# analytical_signal = signal.hilbert(yhpP)
+# plt.plot(tP, yhpP, 'k', label='oscillogram')
+# plt.plot(tP, analytical_signal.real,'b', label = 'real')
+# plt.plot(tP, analytical_signal.imag,'g', label = 'imag')
+
+# amplitude_envelope = np.abs(analytical_signal)
+# plt.plot(tP, amplitude_envelope, 'r', label = 'envelope')
+# plt.rcParams['axes.grid'] = True
+# plt.legend()
+
+#%% plot imporant part of singal:
 fig_processSignal, (proc_Pressure, proc_delta) = plt.subplots(2,1,
                 sharex=True,sharey=False,num='proces Signal')
 fig_processSignal.subplots_adjust(hspace=0)
 
 proc_Pressure.plot(tP,ylpP, 'r', label='pressure')
+proc_delta.plot(tMaxP, deltaP, 'b', label='time since last maxima')
 proc_delta.plot(tP, yhpP, 'g', label='oscillogram')
 
 proc_Pressure.legend()
@@ -151,15 +203,15 @@ fig_timeSignal, (time_filt, time_LP, time_HP) = plt.subplots(3,1,
                 sharex=True,sharey=False,num='mmHg Signal')
 fig_timeSignal.subplots_adjust(hspace=0)
 
-time_filt.plot(t,ymmHg, 'b', label='v')
-time_LP.plot(t, yfLP, 'r')
+time_filt.plot(t,ymmHg, 'b', label='raw')
+time_LP.plot(t, yfLP, 'r', label='LP')
 time_LP.plot(t[localMax], yfLP[localMax], "x")
 time_HP.plot(t,yfHP, 'g', label='HP')
 time_HP.plot(tMaximas, oscMax, 'm', label='local maximas')
 time_HP.plot(tMaximas, deltaT, 'c', label='time since last maxima')
 time_HP.plot(tMaximas, delta2T, 'k', label='2nd derrivative in time')
-time_HP.axvline(x = oscStart)
-time_HP.axvline(x = oscEnd)
+time_HP.axvline(x = tStart)
+time_HP.axvline(x = tEnd)
 
 time_filt.legend()
 time_LP.legend()