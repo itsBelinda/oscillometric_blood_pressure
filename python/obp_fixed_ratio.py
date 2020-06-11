pulse and blood pressure calculation in python using the fixed ratio algorithm. tested with sample_07_04.dat (high pulse) and sample_07_10.dat (low pulse)

python/obp_fixed_ratio.py
@@ -20,7 +20,7 @@ from scipy.interpolate import interp1d
 
 #%% Get the data
 
-data = np.loadtxt('../data/sample_07_07.dat')
+data = np.loadtxt('../data/sample_07_04.dat')
 fs= 1000 # Hz
 resolution = 24 # bits
 N = len(data)
@@ -69,7 +69,7 @@ bLP, aLP = signal.butter(6, f5/fs*2, 'lowpass')
 yfLP = signal.lfilter(bLP, aLP, ymmHg)
 
 # 0.5 Hz HP filter
-f05 = 0.5
+f05 = 0.5 # TODO: might be better to set lower ~0.3
 bHP, aHP = signal.butter(2, f05/fs*2, 'highpass')
 yfHP = signal.lfilter(bHP, aHP, yfLP)
 
@@ -77,7 +77,8 @@ yfHP = signal.lfilter(bHP, aHP, yfLP)
 
 #%% Find peaks in derrivative (HP filtered)
 
-localMax, _ = signal.find_peaks(yfHP, prominence = 0.1 )#distance = 500)
+localMax, _ = signal.find_peaks(yfHP, prominence = 0.3 )#distance = 500)
+
 
 # get values of local maximas in pressure and time
 yMaximas = yfLP[localMax]
@@ -90,6 +91,13 @@ xPumpedUp = np.argmax(yMaximas)
 yPumpedUP = yMaximas[xPumpedUp]
 # the start time of the deflation 
 tPumpedUP = tMaximas[xPumpedUp]
+
+localMin, _ = signal.find_peaks(-yfHP, prominence = 0.3 )
+# get values of local maximas in pressure and time
+yMinima = yfLP[localMin]
+tMinima = t[localMin]
+# the local max values of the oscillation
+oscMin = yfHP[localMin]
 
 
 deltaT = np.zeros(len(tMaximas))
@@ -107,7 +115,7 @@ for i in range(1,len(tMaximas)-1):
     
     if oscStartInd == 0 :
         # check for start of oscillogram: 
-        if np.abs(delta2T[i]) < 0.1 and i > xPumpedUp :
+        if np.abs(delta2T[i]) < 0.2 and i > (xPumpedUp + 5) :
             validCnt += 1
             if validCnt == 5 :
                 oscStartInd = i - (validCnt-1)
@@ -115,11 +123,13 @@ for i in range(1,len(tMaximas)-1):
             validCnt = 0
     elif oscEndInd == 0:
         #check for end of oscillogram
-        if np.abs(delta2T[i]) > 0.1:
-            oscEndInd = i-2 #omitt the last one: -2 not -1       
+        # if np.abs(delta2T[i]) > 0.2:
+        #     oscEndInd = i-3 #omitt the last one: -2 not -1       
+        if (oscMax[oscStartInd]*1.2) > oscMax[i]: # more info on left side
+            oscEndInd = i
 
 if oscEndInd == 0:
-    oscEndInd = len(tMaximas)-2
+    oscEndInd = len(tMaximas)-3
 
 # data for processing:
 tStart = tMaximas[oscStartInd]
@@ -133,59 +143,121 @@ tP = t[iStart:iEnd+1]
 yhpP = yfHP[iStart:iEnd+1]
 ylpP = yfLP[iStart:iEnd+1]
 
+# make sure minimas are (at least) defined for every maxima
+minStart = np.argmax(tMinima>tMaximas[oscStartInd])-1
+minEnd = np.argmax(tMinima>tMaximas[oscEndInd])
+tMinP = tMinima[minStart:minEnd+1]
+oscMinP = oscMin[minStart:minEnd+1]
+
+dMaxMin = oscMaxP - oscMinP[1:len(tMaxP)+1]
+
 avPulse = np.average(deltaP)
 pulse=60/avPulse
+print("Pulse: ", np.around(pulse, decimals=1))
 
-xMAP = np.argmax(yhpP)
+# only use max values
+argMax = np.argmax(oscMaxP)
 
-# Todo: check again and include minimas
-pMAP = ylpP[xMAP]
-pMAPav = np.average(ylpP[xMAP-int(avPulse*1000/2):xMAP+int(avPulse*1000/2)]) #averaged over vone pulse
+pMAP = yfLP[int(round(tMaxP[argMax]*1000))] # simple version: just take highest value
+#pMAP = yfLP[int(round(tMaxP[np.argmax(oscMaxP)]*1000))] #equal
+#old:pMAPav = np.average(ylpP[maxArg-int(avPulse*1000/2):maxArg+int(avPulse*1000/2)]) #averaged over one pulse
+
+# just look at the max value
+print("MAP (max value): ", np.around(pMAP, decimals=2))
+#print("MAP (av value over pulse len): ", np.around(pMAPav, decimals=2))
+          
+searchSys = np.max(oscMaxP)*0.55
+indS = np.argmax(oscMaxP > searchSys)
+dt = tMaxP[indS]-tMaxP[indS-1]
+dosc = oscMaxP[indS]-oscMaxP[indS-1]
+pSBPmax = yfLP[int(round((((searchSys-oscMaxP[indS-1])*dt/dosc)+tMaxP[indS-1])*1000))]
+
+searchDia = np.max(oscMaxP)*0.70
+indD = argMax + np.argmax(oscMaxP[argMax:] < searchDia)
+dt = tMaxP[indD]-tMaxP[indD-1]
+dosc = oscMaxP[indD]-oscMaxP[indD-1]
+pDBPmax = yfLP[int(round((((searchDia-oscMaxP[indD-1])*dt/dosc)+tMaxP[indD-1])*1000))]
+print("    SBP: ", np.around(pSBPmax, decimals=2) )
+print("    DBP: ", np.around(pDBPmax, decimals=2) )
+
+
+#use min and max values (dMaxMin)
+
+argMax = np.argmax(dMaxMin)
+
+pMAPdif = yfLP[int(round(tMaxP[argMax]*1000))] # simple version: just take highest value
+
+# just look at the max value
+print("MAP (max-min value): ", np.around(pMAPdif, decimals=2))
+#print("MAP (av value over pulse len): ", np.around(pMAPav, decimals=2))
+          
+searchSys = np.max(dMaxMin)*0.55
+indS = np.argmax(dMaxMin > searchSys)
+dt = tMaxP[indS]-tMaxP[indS-1]
+dosc = dMaxMin[indS]-dMaxMin[indS-1]
+pSBPmax = yfLP[int(round((((searchSys-dMaxMin[indS-1])*dt/dosc)+tMaxP[indS-1])*1000))]
+
+searchDia = np.max(dMaxMin)*0.70
+indD = argMax + np.argmax(dMaxMin[argMax:] < searchDia)
+dt = tMaxP[indD]-tMaxP[indD-1]
+dosc = dMaxMin[indD]-dMaxMin[indD-1]
+pDBPmax = yfLP[int(round((((searchDia-dMaxMin[indD-1])*dt/dosc)+tMaxP[indD-1])*1000))]
+print("    SBP: ", np.around(pSBPmax, decimals=2) )
+print("    DBP: ", np.around(pDBPmax, decimals=2) )
 
 
 
-print("Pulse: ", int(pulse))
-print("MAP (max value): ", int(pMAP))
-print("MAP (av value over pulse len): ", int(pMAPav))
+#interpolation
+intMax = interp1d(tMaxP, oscMaxP, kind='linear')
+intMin = interp1d(tMinP, oscMinP, kind='linear')
 
+omweInter = intMax(tP) - intMin(tP)
+pMAPinter = ylpP[np.argmax(omweInter)]
+print("MAP (interpolation): ", np.around(pMAPinter, decimals=2) )
 
-interp = interp1d(tMaxP, oscMaxP, kind='quadratic')
+maxArg = np.argmax(omweInter)
+pSBPInt = ylpP[np.argmax(omweInter > np.max(omweInter)*0.55)]
+pDBPInt = ylpP[maxArg + np.argmax(omweInter[maxArg:] < np.max(omweInter)*0.70)]
+print("    SBP: ", np.around(pSBPInt, decimals=2) )
+print("    DBP: ", np.around(pDBPInt, decimals=2) )
 
+#poly fitting
+polyFit = np.polyfit(tMaxP, dMaxMin,4)
+poly = np.poly1d(polyFit)
+omwePoly = poly(tP)
 
-# plt.plot(ylpP, yhpP)
-# plt.figure()
-# plt.plot(tMaxP, oscMaxP)
-# plt.plot(tP, yhpP)
-# plt.plot(tP, interp(tP))
-# plt.figure()
-# plt.plot(tMaxP, oscMaxP)
-# plt.plot(tP, yhpP)
-# plt.plot(tP, interp(tP))
-# proc_delta.plot(tP, interp(tP), 'c', label='interpolate')
+pMAPpoly = ylpP[np.argmax(omwePoly)]
+print("MAP (polyfit): ", np.around(pMAPpoly, decimals=2) )
 
-# envelope only works for DC free signals (remove by additional filtering?)
-# plt.figure('envelope')
-# analytical_signal = signal.hilbert(yhpP)
-# plt.plot(tP, yhpP, 'k', label='oscillogram')
-# plt.plot(tP, analytical_signal.real,'b', label = 'real')
-# plt.plot(tP, analytical_signal.imag,'g', label = 'imag')
+maxArg = np.argmax(omwePoly)
+pSBPpoly = ylpP[np.argmax(omwePoly > np.max(omwePoly)*0.55)]
+pDBPpoly = ylpP[maxArg + np.argmax(omwePoly[maxArg:] < np.max(omwePoly)*0.70)]
+print("    SBP: ", np.around(pSBPpoly, decimals=2) )
+print("    DBP: ", np.around(pDBPpoly, decimals=2) )
 
-# amplitude_envelope = np.abs(analytical_signal)
-# plt.plot(tP, amplitude_envelope, 'r', label = 'envelope')
-# plt.rcParams['axes.grid'] = True
-# plt.legend()
 
 #%% plot imporant part of singal:
-fig_processSignal, (proc_Pressure, proc_delta) = plt.subplots(2,1,
+fig_processSignal, (proc_Pressure, proc_delta, proc_omve) = plt.subplots(3,1,
                 sharex=True,sharey=False,num='proces Signal')
 fig_processSignal.subplots_adjust(hspace=0)
 
 proc_Pressure.plot(tP,ylpP, 'r', label='pressure')
-proc_delta.plot(tMaxP, deltaP, 'b', label='time since last maxima')
+proc_Pressure.plot(tP,ylpP-yhpP, 'b', label='deflation')
+proc_delta.plot(tMaxP, deltaP, 'c', label='time since last maxima')
 proc_delta.plot(tP, yhpP, 'g', label='oscillogram')
+proc_delta.plot(tMaxP, oscMaxP, 'm', label='envelope')
+proc_delta.plot(tMinP, oscMinP, 'm')
+
+proc_delta.plot(tP, intMax(tP), 'b', label='OMV interp.')
+proc_delta.plot(tP, intMin(tP), 'b')
+
+proc_omve.plot(tP, intMax(tP)-intMin(tP), 'g', label='OMVE interp.')
+proc_omve.plot(tMaxP, dMaxMin, 'b', label='OMVE from min/max')
+proc_omve.plot(tP, omwePoly, 'm', label='OMVE polyfit')
 
 proc_Pressure.legend()
 proc_delta.legend()
+proc_omve.legend()
 
 fig_processSignal.suptitle('Blood Pressure', fontsize=20) 
 proc_Pressure.set_ylabel('mmHg', fontsize=16)
@@ -205,9 +277,13 @@ fig_timeSignal.subplots_adjust(hspace=0)
 
 time_filt.plot(t,ymmHg, 'b', label='raw')
 time_LP.plot(t, yfLP, 'r', label='LP')
-time_LP.plot(t[localMax], yfLP[localMax], "x")
+# time_LP.plot(t[localMax], yfLP[localMax], "x")
+# time_LP.plot(t[localMin], yfLP[localMin], "x")
+# time_HP.plot(t[localMax], yfHP[localMax], "x")
+# time_HP.plot(t[localMin], yfHP[localMin], "x")
 time_HP.plot(t,yfHP, 'g', label='HP')
 time_HP.plot(tMaximas, oscMax, 'm', label='local maximas')
+time_HP.plot(tMinima, oscMin, 'm', label='local minimas')
 time_HP.plot(tMaximas, deltaT, 'c', label='time since last maxima')
 time_HP.plot(tMaximas, delta2T, 'k', label='2nd derrivative in time')
 time_HP.axvline(x = tStart)

python/obp_tests.py
@@ -12,7 +12,7 @@ plt.rcParams['axes.grid'] = True
 
 #%% Get the data
 
-data = np.loadtxt('../data/sample_07_10.dat')
+data = np.loadtxt('../data/sample_07_05.dat')
 fs= 1000 # Hz
 resolution = 24 # bits
 N = len(data)