73635f6c5a2f32af71961ea993822944509127ad
itsBelinda <belinda.kneubuehler@outlook.com>
2020-06-23 14:05:52 +0100

    extended interpolation method taking into account the distance between two points, gives worse results

--- python/obp_derrivative.py
+++ python/obp_derrivative.py
@@ -184,11 +184,30 @@ intMax = interp1d(yMaximasP, oscMaxP, kind='quadratic')
 intMin = interp1d(yMinimasP, oscMinP, kind='quadratic')
 
 omweInter = intMax(ybpP)-intMin(ybpP)
-dIPres = np.diff(omweInter)
-dIPres = np.append(0,dIPres)
-
 argMaxInter = np.argmax(omweInter)
 pMAPinter = ybpP[argMaxInter]
+
+# delata alculated by hand and normalised in pressure
+dPresInter = np.zeros(len(omweInter))
+for i in range(0,len(omweInter)-1):
+    dPresInter[i] = (omweInter[i+1]-omweInter[i]) / (ybpP[i+1]-ybpP[i])
+    
+
+# delata alculated by hand and normalised in pressure
+maxChangeInter = np.argmax(dPresInter[0:argMaxInter])
+minChangeInter = np.argmin(dPresInter[argMaxInter:])+argMaxInter
+
+pSBPInter = ybpP[maxChangeInter]
+pDBPInter = ybpP[minChangeInter]
+
+print("interpolation (by hand, normalised in pressure): " )
+print("    MAP: ", np.around(pMAPinter, decimals=2) )
+print("    SBP: ", np.around(pSBPInter, decimals=2) )
+print("    DBP: ", np.around(pDBPInter, decimals=2) )
+
+# delata alculated by python
+dIPres = np.diff(omweInter)
+dIPres = np.append(0,dIPres)
 
 maxChangeI = np.argmax(dIPres[0:argMaxInter])
 minChangeI = np.argmin(dIPres[argMaxInter:])+argMaxInter
@@ -196,8 +215,7 @@ minChangeI = np.argmin(dIPres[argMaxInter:])+argMaxInter
 pSBPInt = ybpP[maxChangeI]
 pDBPInt = ybpP[minChangeI]
 
-print("interpolation: " )
-maxArg = np.argmax(omweInter)
+print("interpolation (by pyhton, not normalised): " )
 print("    MAP: ", np.around(pMAPinter, decimals=2) )
 print("    SBP: ", np.around(pSBPInt, decimals=2) )
 print("    DBP: ", np.around(pDBPInt, decimals=2) )
@@ -225,6 +243,7 @@ proc_delta.plot(yMaximasP, dPres,  label='dOMVE')
 proc_delta.plot(yMaximasP, dPresN,  label='dOMVE n in time')
 proc_delta.plot(yMaximasP, dPresNP,  label='dOMVE n in pressure')
 proc_delta.plot(ybpP, dIPres,  label='dOMVE interpolation')
+proc_delta.plot(ybpP, dPresInter,  label='dOMVE interpolation n in pressure')
 
 
 proc_Pressure.legend()
@@ -247,32 +266,32 @@ plt.get_current_fig_manager().window.showMaximized()
 
 #%% Plot full signals
 #(dMaxMin[i]-dMaxMin[i-1])/tMaxP[i]
-fig_timeSignal, (time_filt, time_LP, time_HP) = plt.subplots(3,1,
-                sharex=True,sharey=False,num='mmHg Signal')
-fig_timeSignal.subplots_adjust(hspace=0)
+# fig_timeSignal, (time_filt, time_LP, time_HP) = plt.subplots(3,1,
+#                 sharex=True,sharey=False,num='mmHg Signal')
+# fig_timeSignal.subplots_adjust(hspace=0)
 
-time_filt.plot(t,ymmHg, 'b', label='raw')
-time_LP.plot(t, yfLP, 'r', label='LP')
-time_HP.plot(t,yfHP, 'g', label='HP')
-time_HP.plot(tMaximas, oscMax, 'm', label='local maximas')
-time_HP.plot(tMinima, oscMin, 'm', label='local minimas')
-time_HP.plot(tMaximas, deltaT, 'c', label='time since last maxima')
-time_HP.plot(tMaximas, delta2T, 'k', label='2nd derrivative in time')
-time_HP.axvline(x = (iStart/1000))
-time_HP.axvline(x = (iEnd/1000))
+# time_filt.plot(t,ymmHg, 'b', label='raw')
+# time_LP.plot(t, yfLP, 'r', label='LP')
+# time_HP.plot(t,yfHP, 'g', label='HP')
+# time_HP.plot(tMaximas, oscMax, 'm', label='local maximas')
+# time_HP.plot(tMinima, oscMin, 'm', label='local minimas')
+# time_HP.plot(tMaximas, deltaT, 'c', label='time since last maxima')
+# time_HP.plot(tMaximas, delta2T, 'k', label='2nd derrivative in time')
+# time_HP.axvline(x = (iStart/1000))
+# time_HP.axvline(x = (iEnd/1000))
 
-time_filt.legend()
-time_LP.legend()
-time_HP.legend()
+# time_filt.legend()
+# time_LP.legend()
+# time_HP.legend()
 
-fig_timeSignal.suptitle('Blood Pressure', fontsize=20) 
-time_filt.set_ylabel('mmHg', fontsize=16)
-time_LP.set_ylabel('mmHg', fontsize=16)
-time_HP.set_ylabel('detlat/mmHg', fontsize=16)
-time_HP.set_xlabel('Time (s)', fontsize=16)
-time_LP.set_xlim(0, max(t))
-time_HP.set_ylim(-3, 3)
-plt.get_current_fig_manager().window.showMaximized()
+# fig_timeSignal.suptitle('Blood Pressure', fontsize=20) 
+# time_filt.set_ylabel('mmHg', fontsize=16)
+# time_LP.set_ylabel('mmHg', fontsize=16)
+# time_HP.set_ylabel('detlat/mmHg', fontsize=16)
+# time_HP.set_xlabel('Time (s)', fontsize=16)
+# time_LP.set_xlim(0, max(t))
+# time_HP.set_ylim(-3, 3)
+# plt.get_current_fig_manager().window.showMaximized()
  
 
 
